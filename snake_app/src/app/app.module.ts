82d5ff5c848d3be9a458b8ca4c09a1c12a98a353
Maciej Jarocki <mmaciej.jarocki@gmail.com>
2023-01-26 12:27:37 +0100

    added home-page-form component

--- snake_app/src/app/app.module.ts
+++ snake_app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageFormComponent } from './home-page-form/home-page-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageFormComponent
   ],
   imports: [
     BrowserModule
