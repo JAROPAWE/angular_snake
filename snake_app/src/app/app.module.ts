720979b7cb85f1d755c8837c2359aa068daf2386
Maciej Jarocki <mmaciej.jarocki@gmail.com>
2023-01-27 08:13:52 +0100

    added game component

--- snake_app/src/app/app.module.ts
+++ snake_app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomePageFormComponent } from './home-page-form/home-page-form.component';
+import { GameComponent } from './game/game.component';
 // import { EmailValidatorDirective } from './email-validator.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageFormComponent,
+    GameComponent,
     // EmailValidatorDirective
   ],
   imports: [
